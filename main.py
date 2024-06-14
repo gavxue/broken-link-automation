51d133d604bc8446bc47359e29a1f5c65537a400
Gavin Xue <gavinxue050705@gmail.com>
2024-06-14 16:17:30 -0400

    Added color to text-based output interface

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import requests
+from colored import fore, back, style
 
 driver = webdriver.Chrome()
 driver.get("https://uwaterloo.ca/civil-environmental-engineering-information-technology")
+
+broken_links = 0
+
+# colours
+color_success = f"{fore('green')}"
+color_error = f"{fore('red')}"
+color_warning = f"{fore('yellow')}"
+color_menu = f"{style('bold')}{fore('magenta')}{back('yellow')}"
+color_submenu = f"{style('bold')}{fore('blue')}"
+color_prompt = f"{fore('cyan')}"
 
 
 def pause():
@@ -13,25 +24,40 @@ def pause():
 
 
 def check_status(link):
+    global broken_links
+
+    text = link.text
+    status = "HTTP 200 OK"
+    color = color_success
+
     url = link.get_attribute('href')
 
     if not url:
-        print('error')
+        status = "ERROR. LINK WITH NO HREF"
+        color = color_error
+        broken_links += 1
     elif 'mailto' in url:
-        print('mail')
+        status = "MAIL LINK (NEEDS MANUAL CHECK)"
+        color = color_warning
     else:
         try:
             response = requests.get(url, timeout=10)
-            print(url, response.status_code)
+            if response.status_code != 200:
+                color = color_error
+                status = "HTTP " + str(response.status_code)
+                broken_links += 1
         except requests.exceptions.Timeout:
-            print('timed out')
+            color = color_error
+            status = "HTTP 408 Request Timeout"
+            broken_links += 1
+
+    print(f"{color}{text} - {status}{style('reset')}")
         
 
 def page():
     main = driver.find_element(By.TAG_NAME, 'main')
     links = main.find_elements(By.TAG_NAME, 'a')
 
-    print('PAGE')
     for link in links:
         check_status(link)
 
@@ -39,10 +65,15 @@ def page():
 def subsubmenu(subnav, subsubnav):
     subsubnav_items = subsubnav.find_elements(By.XPATH, '../ul/li/a')
     
+    # navigates through subsub menu (lol)
     for subsubnav_item in subsubnav_items:
+        text = subsubnav_item.find_element(By.TAG_NAME, 'span').get_attribute('textContent').strip()
+        print(f"{color_submenu}--- {text} ---{style('reset')}")
+
         subnav.click()
         pause()
         driver.get(subsubnav_item.get_attribute('href'))
+        page()
         pause()
         driver.back()
         pause()
@@ -51,7 +82,11 @@ def subsubmenu(subnav, subsubnav):
 def submenu(subnav):
     subnav_items = subnav.find_elements(By.XPATH, '../ul/li/a')
     
+    # navigates through sub menu
     for subnav_item in subnav_items:
+        text = subnav_item.find_element(By.TAG_NAME, 'span').get_attribute('textContent').strip()
+        print(f"{color_submenu}--- {text} ---{style('reset')}")
+
         li = subnav_item.find_element(By.XPATH, '..')
 
         subnav.click()
@@ -73,6 +108,9 @@ def menu():
 
     # navigates through menu
     for nav_item in nav_items:
+        text = nav_item.find_element(By.TAG_NAME, 'span').text.upper()
+        print(f"{color_menu}===== {text} ====={style('reset')}")
+
         if 'menu__link-sub' in nav_item.get_attribute('class'):
             submenu(nav_item)
         else:
@@ -81,5 +119,15 @@ def menu():
             driver.back()
         
 
-if __name__ == '__main__':
+def main():
+    print(broken_links)
+    print(f"{color_prompt}Starting broken link check automation...{style('reset')}")
     menu()
+    print(f"{color_prompt}Done!{style('reset')}")
+
+    color = color_success if broken_links == 0 else color_error
+
+    print(f"There were {color}{str(broken_links)} broken links{style('reset')}.")
+
+if __name__ == '__main__':
+    main()
