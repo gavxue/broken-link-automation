992c702d73d34f0454d5370b03040b8d14b9414d
Gavin Xue <gavinxue050705@gmail.com>
2024-06-17 16:25:31 -0400

    Added check page

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
+
+
 app = Flask(__name__)
+
+@app.route('/check')
+def check():
+    return render_template('check.html')
+
 
 @app.route('/')
 def index():
-    return render_template('')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
