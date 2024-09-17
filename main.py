eb30aab0ba16f12adba3d3120e789d3a76a8c536
Gavin Xue <gavinxue050705@gmail.com>
2024-09-17 00:34:53 -0400

    Added comments

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import requests
 from colored import fore, back, style
 
-
+# url of site to check
 url = "https://uwaterloo.ca/civil-environmental-engineering-information-technology"
 # url = "https://uwaterloo.ca/civil-environmental-engineering"
 # url = "https://uwaterloo.ca/engineering/"
@@ -14,6 +14,7 @@ url = "https://uwaterloo.ca/civil-environmental-engineering-information-technolo
 driver = webdriver.Chrome()
 driver.get(url)
 
+# counter
 broken_links = 0
 
 # colours
@@ -30,17 +31,18 @@ headers = {
     'accept': 'application/json'
 }
 
-# class
+# link object
 class Link:
     def __init__(self, title, url):
         self.title = title
         self.url = url
 
-
+# pause function
 def pause():
     time.sleep(0.5)
 
 
+# checks a single link
 def check_status(link):
     global broken_links
 
@@ -51,27 +53,34 @@ def check_status(link):
     url = link.get_attribute('href')
 
     if not url:
-        status = "ERROR. LINK WITH NO HREF"
+        # no url
+        status = "ERROR. LINK WITH NO URL"
         color = color_error
         broken_links += 1
     elif 'mailto' in url:
+        # mail link
         status = "MAIL LINK (NEEDS MANUAL CHECK)"
         color = color_warning
     else:
         try:
+            # send request
             response = requests.get(url, timeout=10, headers=headers)
             if response.status_code == 403 or response.status_code == 401:
+                # authorization required
                 color = color_warning
                 status = "AUTHORIZATION REQUIRED FOR LINK (NEEDS MANUAL CHECK)"
             elif response.status_code != 200:
+                # not successful
                 color = color_error
                 status = "HTTP " + str(response.status_code)
                 broken_links += 1
         except requests.exceptions.Timeout:
+            # request timed out
             color = color_error
             status = "HTTP 408 Request Timeout"
             broken_links += 1
         except:
+            # unknown error
             color = color_error
             status = "Unknown error occured (needs manual check)"
             broken_links += 1
@@ -79,26 +88,32 @@ def check_status(link):
     print(f"{color}{text} - {status}{style('reset')}")
         
 
+# checks a single page
 def page():
+    # get page main content
     main = driver.find_element(By.TAG_NAME, 'main')
     links = main.find_elements(By.TAG_NAME, 'a')
 
+    # check all links on page
     for link in links:
         check_status(link)
 
 
+# iterates through menu
 def menu():
     nav = driver.find_elements(By.TAG_NAME, "nav")
    
     a = nav[2].find_elements(By.TAG_NAME, 'a')
     if len(nav) == 4:
-        a = a + nav[3].find_elements(By.TAG_NAME, 'a')
+        a = a + nav[3].find_elements(By.TAG_NAME, 'a')      # if there is a secondary nav
     
+    # get all menu items
     nav_items = []
     for link in a:
         if link.get_attribute('href'):
             nav_items.append(Link(link.get_attribute('textContent').strip(), link.get_attribute('href')))
 
+    # check each menu item
     for nav_item in nav_items:
         print(f"{color_menu}--- {nav_item.title} ---{style('reset')}")
         driver.get(nav_item.url)
@@ -113,6 +128,7 @@ def main():
     color = color_success if broken_links == 0 else color_error
 
     print(f"There were {color}{str(broken_links)} broken links{style('reset')}.")
+
 
 if __name__ == '__main__':
     main()
